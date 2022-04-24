413675a47d3e5a7533fd03ac039e4ab8e1b1ca47
André Bering <andre@bering.in>
2022-04-24 14:09:12 +0200

    docker setup - added docker setup - extended readme - added GitHub workflow - added doc strings - switched to logger instead of print

--- README.md
+++ README.md
@@ -1,13 +1,58 @@
-# run debug
+# Introduction
 
+This project can be used to auto protect the master/main branch of newly created GitHub projects. The protection process is triggered by [GitHub Webhooks](https://docs.github.com/en/developers/webhooks-and-events/webhooks/about-webhooks). Please note that this is only possible for projects inside an [Organizations](https://docs.github.com/en/organizations). Because only organizations provide the necessary [webhook](https://docs.github.com/en/developers/webhooks-and-events/webhooks/webhook-events-and-payloads#project) for acting upon project creation.
+
+The current implementation does only act upon the action `created`. Any other actions are ignored.
+
+# Development
+
+## Run debug
+
+Clone the project and perform the following steps. Replace the values in brackets (removing the brackets) with correspondig values for your environment.
+
+    cd <project-name>
+    pip install -r requirements.txt
     export FLASK_APP=project
     export FLASK_DEBUG=1
-    export GITHUB_TOKEN=OAUTH_TOKEN
-    export GITHUB_OWNER=GitHub_Owner_Name
-    export GITHUB_SIGNATURE=Webhook_Signature
-    export APP_URL=http://localhost:5000
+    export GITHUB_TOKEN=<OAUTH_TOKEN>
+    export GITHUB_OWNER=<GitHub_Owner_Name>
+    export GITHUB_SECRET=<Webhook_Secret>
+    export APP_URL=<http://localhost:5000>
     flask run
 
-# freeze requirements
+During development it could be easier to run this locally and make the webhook publicly available using a service like [ngrok](https://ngrok.com/). Please consult their [documentation](https://ngrok.com/docs) on which steps are necessary to do this.
+
+## Freeze requirements
+
+When addtional requirements are added to the project please use the following command to update the requirements file.
 
     pip freeze | grep -v "pkg-resources" > requirements.txt
+
+# Production
+
+## Run
+
+Build the docker image and make it available to your server.
+For running the docker image the provided `docker-compose.yml` can be used as a starting point.
+The `.env.sample` can be used as a starting point for the needed `.env` file.
+
+**It is important that the variable `FLASK_ENV` is set to `production` and `FLASK_DEBUG` to `0`. In debug mode the signature validation is bypassed which is not recommend in a production environment.**
+
+The following values must be changed for your individual needs/environment.
+
+| environment variable | description | example |
+|--|--|--|
+| GITHUB_TOKEN | The [personal access token](https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token) (OAuth) for GitHub which can used to interact with the API | abcdefghijklmnopqrstuvwxyz_0123456789abc |
+| GITHUB_OWNER | The GitHub handle of the person which should be mentioned | eberdna |
+| GITHUB_SECRET | The secret which is configured for the webhook at GitHub | Test1234 |
+| APP_URL | The URL under which the webhook application publicly accessible | https://myhook.example.org |
+
+## Configure Webhook
+
+If the example `APP_URL` from the example would be used, you need to set the »Payload URL« of the webhook in the GitHub configuration of your Organization to the following endpoint: `https://myhook.example.org/hooktest`. In addition the »Content Type« must be set to `application/json`.
+
+The only relevant event is »Repositories«. All other events must be unchecked during configuration.
+
+# Known Issues
+
+- The provided `default_branch` in the webhook payload is `master` even though the setting at organization level is set to `main`. Therefore the value for `default_branch` is hard coded to `main`. This must be changed once the payload does include the right `default_branch` information.
--- project/__init__.py
+++ project/__init__.py
@@ -2,11 +2,12 @@ import hmac
 import hashlib
 
 import time
+
 import requests  # type: ignore
 import os
 import base64
-from pprint import pprint
 from flask import Flask, request, json, jsonify
+
 
 app = Flask(__name__, static_url_path="/static", static_folder="web/static")
 
@@ -17,17 +18,34 @@ policy_url = app_url + "/static/restrict.json"
 
 @app.route("/", methods=["GET", "POST"])
 def hello_world():
+    """endpoint for availability check"""
     if request.method == "GET":
+        app.logger.info("received request")
         return "<p>Hello, World!</p>"
     if request.method == "POST":
         return "<p>Hello, Post-World!</p>"
 
 
 def create_initial_commit(base_query_url, headers, branchname):
-    # create README for first commit
+    """create README for first commit
+
+    Parameters
+    ----------
+    base_query_url: str
+    headers: dict[str, str]
+        headers for GitHub API request, must include auth token and accepted content type
+    branchname: str
+        name of branch where commit should be created
+
+    Returns
+    ------
+    str
+        full qualified url of pointing to created commit
+    """
     content = {}
     content["branch"] = branchname
     content["message"] = "This is a placeholder for security purposes"
+    app.logger.info(f"loading readme from {readme_url}")
     readme_request = requests.get(readme_url)
     base64_bytes = base64.b64encode(readme_request.content)
     content["content"] = base64_bytes.decode("utf-8")
@@ -36,53 +54,73 @@ def create_initial_commit(base_query_url, headers, branchname):
         headers=headers,
         data=json.dumps(content),
     )
-    print(blob_request.request.body)
-    print(blob_request.request.headers)
-    print(blob_request.content)
+
+    app.logger.log(10, blob_request.request.body)
+    app.logger.log(10, blob_request.request.headers)
+    app.logger.log(10, blob_request.content)
     blob_sha = blob_request.json()["commit"]["tree"]["sha"]
     commit_sha = blob_request.json()["commit"]["sha"]
     commit_html_url = blob_request.json()["commit"]["html_url"]
-    print(f"blob_request.status_code: {blob_request.status_code}")
+    app.logger.log(10, f"blob_request.status_code: {blob_request.status_code}")
 
-    print(f"blob_sha: {blob_sha}")
-    print(f"commit_sha: {commit_sha}")
+    app.logger.log(10, f"blob_sha: {blob_sha}")
+    app.logger.log(10, f"commit_sha: {commit_sha}")
     commit_data = {}
     commit_data["message"] = "initial commit"
     commit_data["tree"] = blob_sha
     commit_request = requests.post(
         base_query_url + "/git/commits", headers=headers, data=json.dumps(commit_data)
     )
-    print(commit_request.content)
-    print(f"commit_request.status_code: {commit_request.status_code}")
+    app.logger.log(10, commit_request.content)
+    app.logger.log(10, f"commit_request.status_code: {commit_request.status_code}")
 
     p_commit_data = {}
     p_commit_data["force"] = True
     p_commit_request = requests.post(
         base_query_url + "", headers=headers, data=json.dumps(p_commit_data)
     )
-    print(p_commit_request.content)
-    print(f"p_commit_request.status_code: {p_commit_request.status_code}")
+    app.logger.log(10, p_commit_request.content)
+    app.logger.log(10, f"p_commit_request.status_code: {p_commit_request.status_code}")
     return commit_html_url
 
 
 def restrict_commits(base_query_url, headers, branch_name):
+    """create branch protection
+
+    Parameters
+    ----------
+    base_query_url: str
+    headers: dict[str, str]
+        headers for GitHub API request, must include auth token and accepted content type
+    branch_name: str
+        name of branch which should be protected
+    """
     req_url = base_query_url + f"/branches/{branch_name}/protection"
-    print(req_url)
+    app.logger.log(10, req_url)
+    app.logger.info(f"loading policy from {policy_url}")
     policy_def = requests.get(policy_url)
     parsed = json.loads(policy_def.content.decode("utf8"))
     restrict_request = requests.put(req_url, headers=headers, data=json.dumps(parsed))
-    print(restrict_request.content)
-    print(f"restrict_request.status_code: {restrict_request.status_code}")
-    return restrict_request.content
+    app.logger.log(10, restrict_request.content)
+    app.logger.log(10, f"restrict_request.status_code: {restrict_request.status_code}")
 
 
 def create_issue(base_query_url, headers, commit_url):
-    """create an issue with the policy content and a mention"""
+    """create an issue with the policy content and a mention
+
+    Parameters
+    ----------
+    base_query_url: str
+    headers: dict[str, str]
+        headers for GitHub API request, must include auth token and accepted content type
+    commit_url: str
+        full qualified url pointing to the previously created commit
+    """
     data = {}
     data["title"] = "Created branch protection rule"
     policy_def = requests.get(policy_url)
     parsed = json.loads(policy_def.content.decode("utf8"))
-    print(json.dumps(parsed, indent=4, sort_keys=True))
+    app.logger.log(10, json.dumps(parsed, indent=4, sort_keys=True))
     data["body"] = (
         "@"
         + os.getenv("GITHUB_OWNER")
@@ -101,14 +139,14 @@ def create_issue(base_query_url, headers, commit_url):
             + commit_url
         )
     req_url = base_query_url + f"/issues"
-    print(req_url)
+    app.logger.log(10, req_url)
     issue_request = requests.post(req_url, headers=headers, data=json.dumps(data))
-    print(issue_request.content)
-    print(issue_request.status_code)
+    app.logger.log(10, issue_request.content)
+    app.logger.log(10, issue_request.status_code)
 
 
 def validate_signature(request):
-    key = os.getenv("GITHUB_SIGNATURE")
+    key = os.getenv("GITHUB_SECRET")
     key = bytes(key, "utf-8")
     expected_signature = hmac.new(
         key=key, msg=request.data, digestmod=hashlib.sha1
@@ -130,44 +168,49 @@ def hook_root():
         and not app.debug
     ):
         response = jsonify({"message": "Wrong secret"})
+        app.logger.warn("authentication failed")
         return response, 401
     else:
-        print("passed signature verification")
+        app.logger.info("passed signature verification")
 
-    print(request.headers["X-Github-Event"])
-    if request.headers["Content-Type"] == "application/json":  # calling json objects
+    app.logger.log(10, f'X-Github-Event: {request.headers["X-Github-Event"]}')
+    if request.headers["Content-Type"] == "application/json":
         payload = request.json
-        # print(json.dumps(request.json))
         if (
             request.headers["X-Github-Event"] == "repository"
             and payload["action"] == "created"
         ):
-            print("now do the magic - created")
+            app.logger.log(10, "now do the magic - created")
             token = os.getenv("GITHUB_TOKEN")
             headers = {
                 "Authorization": f"token {token}",
                 "Accept": "application/vnd.github.v3+json",
             }
             repo_name = payload["repository"]["full_name"]
-            default_branch_name = payload["repository"]["default_branch"]
+            # default_branch_name = payload["repository"]["default_branch"]
+            # hard-coded to 'main' because the payload for 'created' includes 'master' as 'default_branch'
+            # even though default branch is set to 'main' in organization settings.
+            default_branch_name = "main"
             base_query_url = f"https://api.github.com/repos/{repo_name}"
             r = requests.get(base_query_url + "/commits", headers=headers)
-            pprint(r.json())
-            print("default_branch:" + default_branch_name)
+            app.logger.log(10, "default_branch:" + default_branch_name)
+            app.logger.log(10, r.status_code)
+            # only create initial commit if repository has not been initialized with content, e.g. README.md
+            commit_url = None
             if r.status_code == 409:
                 commit_url = create_initial_commit(
                     base_query_url, headers, default_branch_name
                 )
             # restrict commits
-            content = restrict_commits(base_query_url, headers, default_branch_name)
+            restrict_commits(base_query_url, headers, default_branch_name)
             # create issue with policy content and mention
             create_issue(base_query_url, headers, commit_url)
 
-        # return json.dumps(request.json)
         response = jsonify({"message": "Success"})
         return response, 200
 
     else:
         response = jsonify({"message": "Unexpected Content-Type"})
+        app.logger.warn("received request with wrong Content-Type")
         # unsupported media type
         return response, 415
